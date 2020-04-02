91df62c5d78c06d71ef30336564914b33337ddc2
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-02 12:19:36 +0530

    Added:-Enter The Valid Name

--- src/main/java/com/userRegistration/User_Registration.java
+++ src/main/java/com/userRegistration/User_Registration.java
@@ -1,9 +1,31 @@
 package com.userRegistration;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+import java.util.Scanner;
+
 public class User_Registration
 {
+    public void validFirstName(String name)
+    {
+        String regex ="^[A-Z][a-z]{3,}$";
+        boolean pattern = name.matches(regex);
+        if (pattern)
+        {
+            System.out.println("Nmae is valid");
+        }
+        else
+        {
+            System.out.println("Name Is Invalid");
+        }
+
+    }
     public static void main(String[] args)
     {
-        System.out.println("Welcome To User Registration Programe");
+        System.out.println("Enter Name : ");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        User_Registration user = new User_Registration();
+        user.validFirstName(name);
     }
 }
