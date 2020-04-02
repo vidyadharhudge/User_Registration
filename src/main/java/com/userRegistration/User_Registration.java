4642fae70725c826835a3c7474916cc8b8b2c092
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-02 16:26:07 +0530

    Added:-Clear All Email Sample

--- src/main/java/com/userRegistration/User_Registration.java
+++ src/main/java/com/userRegistration/User_Registration.java
@@ -10,14 +10,20 @@ public class User_Registration
     {
         String regex = "^[A-Z][a-z]{3,}$";
         boolean pattern = name.matches(regex);
-        if (pattern) {
+        if (pattern)
+        {
             System.out.println("Name is Valid");
-        } else {
+        }
+        else
+        {
             System.out.println("Name Is Invalid");
         }
     }
     public void Validate_The_Email(String name)
     {
+        String emailPattern1 = "^[a-zA-Z0-9]{3,}+[@]{1}$";
+        String emailPattern2 = "^[a-zA-Z0-9]{3,}+[@]{1}[a-zA-Z0-9]{3,}$";
+        String emailPattern3 = "^[a-zA-Z0-9]{3,}+[@]{1}[a-zA-Z0-9]{3,}+[.][a-zA-Z]{3,}$";
         String regex="^[a-zA-Z0-9]{3,}+[@][a-zA-Z0-9]{3,}+[.][a-zA-Z]{3,}$";
         boolean pattern = name.matches(regex);
         if (pattern)
@@ -32,7 +38,7 @@ public class User_Registration
     }
     public void Validate_The_Number(String name)
     {
-        String regex="^[0-9]{2} [6-9]{1}[0-9]{9}$";
+        String regex="^[0-9]{2}[0-9]{10}$";
         boolean pattern = name.matches(regex);
         if (pattern)
         {
@@ -44,10 +50,10 @@ public class User_Registration
         }
 
     }
-    public void Validate_The_Password(String name)
+    public void Validate_The_Password(String name1)
     {
         String regex="^[A-Z]{1}[a-z]{5}[0-9]{3}$";
-        boolean pattern = name.matches(regex);
+        boolean pattern = name1.matches(regex);
         if (pattern)
         {
             System.out.println("password is Valid For Use Case 5");
@@ -88,11 +94,6 @@ public class User_Registration
         }
 
     }
-
-
-
-
-
     public static void main(String[] args)
     {
         System.out.println("Enter First Name : ");
@@ -100,18 +101,23 @@ public class User_Registration
         String firstName=sc.next();
         User_Registration user = new User_Registration();
         user.Validate_The_Name(firstName);
+
         System.out.println("Enter Last Name : ");
         String lastName=sc.next();
         user.Validate_The_Name(lastName);
+
         System.out.println("Enter Last Validate Email : ");
         String Email=sc.next();
         user.Validate_The_Email(Email);
+
         System.out.println("Enter Mobile Number : ");
         String mobileNumber=sc.next();
         user.Validate_The_Number(mobileNumber);
+
         System.out.println("Enter Password : ");
-        String password=sc.next();
-        user.Validate_The_Password (password);
+        String password1=sc.next();
+        user.Validate_The_Password (password1);
+
         System.out.println("Enter Password : ");
         String Password=sc.next();
         user.Upper_Case_Password (Password);
