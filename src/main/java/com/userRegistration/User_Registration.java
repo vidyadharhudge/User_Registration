b03b03266c4fb7eca97624db5374640c16e6474d
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-02 11:01:36 +0530

    Added:-Welcome Message

---
+++ src/main/java/com/userRegistration/User_Registration.java
@@ -0,0 +1,9 @@
+package com.userRegistration;
+
+public class User_Registration
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To User Registration Programe");
+    }
+}
